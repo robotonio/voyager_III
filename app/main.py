1758c931d60652fd063e3a2e99304c2d7a4e712c
Charis Karametos <charis.karametos@outlook.com>
2025-01-24 21:01:58 +0200

    πρεπει να δουλευει αλλα δεν ειμαι σιγουρος γτ δεν εχουμε προσβαση στο google maps api

--- app/main.py
+++ app/main.py
@@ -1,2 +1,6 @@
-# "Hello, World!" example
-print("Hello, World!")
+import asyncio
+from server import Server
+import uvicorn
+app = asyncio.run(Server().create_server())
+
+uvicorn.run(app, host="0.0.0.0", port=3000)
