f99e49c71785ea821d46b5a3d33cc95232497b55
Tassos <kasmiris@robotonio.gr>
2025-01-28 23:23:41 +0200

    LoRaModule

--- cansat/cansat.ino
+++ cansat/cansat.ino
@@ -1,26 +1,52 @@
+#include <SPI.h>
+#include "LoRaModule.h"
 #include "Sensors.h"
 
-// Δημιουργία αντικειμένου Sensors
+// Ορισμός των pins που χρησιμοποιούνται από το transceiver module
+#define SS_PIN 7
+#define RST_PIN 18
+#define DIO0_PIN 38
+#define FREQUENCY 433E6
+#define SYNC_WORD 0xF3
+
+LoRaModule lora(SS_PIN, RST_PIN, DIO0_PIN, FREQUENCY, SYNC_WORD);
 Sensors sensors;
 
 void setup() {
-    Serial.begin(115200);
-    Serial.println("Initializing sensors... Let's go!!!");
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("Initializing sensors... Let's go!!!");
 
-    // Αρχικοποίηση αισθητήρων
-    sensors.initialize();
+  // Αρχικοποίηση αισθητήρων
+  sensors.initialize();
+  Serial.println("Sensor initialization completed! Yeah!!!!!");
 
-    Serial.println("Sensor initialization completed! Yeah!!!!!");
+  // Αρχικοποίηση LoRa
+  Serial.println("LoRa Initializing...");
+  lora.begin();
+  Serial.println("LoRa Initializing OK!");
 }
 
 void loop() {
-    // Ανάγνωση όλων των τιμών
-    sensors.read();
+  // Ανάγνωση όλων των τιμών από τους αισθητήρες
+  sensors.read();
+  std::string sensorDataStd = sensors.to_string();
+  String sensorData = String(sensorDataStd.c_str());
 
-    // Εκτύπωση των τιμών ως string
-    Serial.print("All Sensor Values: ");
-    Serial.println(sensors.to_string().c_str());
+  // Αποστολή δεδομένων μέσω LoRa
+  lora.sendString(sensorData);
+  Serial.println("Sent: " + sensorData);
 
-    Serial.println("----------------------");
-    delay(2000); // Αναμονή 2 δευτερόλεπτα
+  // Λήψη δεδομένων μέσω LoRa
+  String receivedMessage = lora.receiveString();
+  if (receivedMessage != "") {
+    Serial.print("Received packet '");
+    Serial.print(receivedMessage);
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  } else {
+    Serial.println("Received Nothing!");
+  }
+
+  delay(1000); // Αναμονή 1 δευτερόλεπτο
 }
